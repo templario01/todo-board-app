fbc0acea634af381f1d18dd598673fd6939a506a
victor benavente <templario0141@gmail.com>
2022-06-17 10:09:18 -0500

    chore: fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import { Container } from './components/Container'
 import { TaskCreator } from './components/TaskCreator'
 import { TaskTable } from './components/TaskTable'
